bd8b3da9c6efb13c94320dd1503fb09ce730bd57
BhushanMali45 <112244238+BhushanMali45@users.noreply.github.com>
2023-07-23 18:13:30 +0530

    init

--- src/data.js
+++ src/data.js
@@ -11,39 +11,41 @@
 export const featuredPortfolio = [
   {
     id: 1,
-    title: "Temp APP",
-    link:"https://www.google.com",
+    title: "Weather App",
+    link:"https://weathercheck-h62r.onrender.com/",
     img:
       "./assets/1.jpg",
   },
   {
     id: 2,
-    title: "Temp APP",
-    link:"https://649d915a782a83141a37e19d--dreamy-lily-2a43d3.netlify.app/",
+    title: "Placement Management system",
+    link:"https://placement-management-system45.onrender.com/",
     img:
       "./assets/2.jpg",
   },
   {
     id: 3,
-    title: "Temp APP",
-    link:"https://weathercheck-h62r.onrender.com/",
+    title: "Music",
+    link:"https://45music.netlify.app",
     img:
       "./assets/3.jpg",
   },
   {
     id: 4,
-    title: "Placement Management System",
-    link:"https://placement-management-system45.onrender.com/",
+    title: "speech to text",
+    link:"https://speechtotextrecog.netlify.app",
           img: "./assets/4.jpg",
   },
   {
     id: 5,
-    title: "Placement Management System",
+    title: "survey",
+    link:"surveyreact.netlify.app",
     img: "./assets/5.jpg",
   },
   {
     id: 6,
-    title: "Placement Management System",
+    title: "portfolio",
+    link:"https://portfoliobhushan.netlify.app",
     img: "./assets/6.jpg",
   },
 ];
@@ -51,51 +53,49 @@ export const featuredPortfolio = [
 export const webPortfolio = [
   {
     id: 1,
-    title: "Temp APP",
-    link:"https://www.google.com",
-    img:
-      "./assets/1.jpg",
-  },
-  {
-    id: 2,
-    title: "Temp APP",
-    link:"https://www.google.com",
-    img:
-      "./assets/2.jpg",
-  },
-  {
-    id: 3,
-    title: "Temp APP",
-    link:"https://www.google.com",
+    title: "Music",
+    link:"https://45music.netlify.app",
     img:
       "./assets/3.jpg",
   },
+  {
+    id: 2,
+    title: "portfolio",
+    link:"https://portfoliobhushan.netlify.app",
+    img: "./assets/6.jpg",
+  },
+
  
 ];
 
 export const mobilePortfolio = [
   {
     id: 1,
-    title: "Temp APP",
-    link:"https://www.google.com",
-    img:
-      "./assets/1.jpg",
+    title: "survey",
+    link:"surveyreact.netlify.app",
+    img: "./assets/5.jpg",
   },
+  {id: 2,
+    title: "speech to text",
+    link:"https://speechtotextrecog.netlify.app",
+          img: "./assets/4.jpg",
+
+  }
  
 ];
 
 export const designPortfolio = [
   {
     id: 1,
-    title: "Temp APP",
-    link:"https://www.google.com",
+    title: "Weather App",
+    link:"https://weathercheck-h62r.onrender.com/",
     img:
       "./assets/1.jpg",
   },
   {
     id: 2,
-    title: "Temp APP",
-    link:"https://www.google.com",
+    title: "Placement Management system",
+    link:"https://placement-management-system45.onrender.com/",
     img:
       "./assets/2.jpg",
   },
@@ -106,35 +106,32 @@ export const designPortfolio = [
     img:
       "./assets/3.jpg",
   },
-  {
-    id: 4,
-    title: "Placement Management System",
-          img: "./assets/4.jpg",
-  },
+ 
 
 ];
 
 export const contentPortfolio = [
   {
     id: 1,
-    title: "Temp APP",
-    link:"https://www.google.com",
+    title: "Weather App",
+    link:"https://weathercheck-h62r.onrender.com/",
     img:
       "./assets/1.jpg",
   },
   {
     id: 2,
-    title: "Temp APP",
-    link:"https://www.google.com",
+    title: "Placement Management system",
+    link:"https://placement-management-system45.onrender.com/",
     img:
       "./assets/2.jpg",
   },
   {
     id: 3,
-    title: "Temp APP",
-    link:"https://www.google.com",
+    title: "Music",
+    link:"https://45music.netlify.app",
     img:
       "./assets/3.jpg",
+  
   },
  
 ];
