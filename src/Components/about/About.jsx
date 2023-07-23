d2b0d1f8cac7e19373fe30855fce65e636a18cb2
BhushanMali45 <112244238+BhushanMali45@users.noreply.github.com>
2023-07-23 22:56:54 +0530

    init

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -13,43 +13,46 @@ const About = () => {
             className="a-img"
           />
         </div>
+        
       </div>
       <div className="a-right">
+
         <h1 className="a-title">About Me</h1>
+        <div className="resume">
+          <a href="https://drive.google.com/file/d/14AycoucccWSOTdUb5qfxoa0RX2MNqkII/view?usp=drivesdk" className="btn" download> Download Resume </a>
+        </div>
         <p className="a-role">
-        MERN,Full Stack Developer | Alpha Batch Student| Intern At TCR innovation | Intern At Lets GrowMore.
+          MERN,Full Stack Developer | Alpha Batch Student| Intern At TCR innovation | Intern At Lets GrowMore.
         </p>
         <p className="a-desc">
-        .I am experienced in Full Stack for developing  Web Portal Placement Management System And Music website like spotify ( Intern At TCR innvotion as Frontend role and At Let's growMore).
-.
+          .I am experienced in Full Stack for developing  Web Portal Placement Management System And Music website like spotify ( Intern At TCR innvotion as Frontend role and At Let's growMore).
+          .
 
- 
-        </p>
-        <div className="a-pr">
-<b>          I have got  1st price Web Design in Milestone 2k22.
-</b>          <p>I have worked on Frontend technologies (HTML, CSS, BootStraps, Javascript,React Js) and Backend technologies(Node js ,Mongodb And Mysql ) for almost a year and had done several projects.
+
+         </p>
+         <div className="a-pr">
+          <b>I have got  1st price Web Design in Milestone 2k22.</b>
+          <p>I have worked on Frontend technologies (HTML, CSS, BootStraps, Javascript,React Js) and Backend technologies(Node js ,Mongodb And Mysql ) for almost a year and had done several projects.
 
             Skillful in C, C++, Java and Data Structure with hands on experience on web development tools.
-</p>
-          </div>
+          </p>
+        </div> 
 
-          {/* <div  className="f">
+        <div  className="f">
             <p>
 Aspiring for a challenging career in Software Development as to use my learned skills and experience </p>
-          </div> */}
+          </div> 
 
 
 
-          <div className="resume">
-            <a href="https://drive.google.com/file/d/14AycoucccWSOTdUb5qfxoa0RX2MNqkII/view?usp=drivesdk " className="btn" download> Download Resume </a>
-          </div>
-        </div>
+        
       </div>
+    </div>
 
 
 
 
-   
+
   );
 };
 
